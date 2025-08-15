e7bb12ebc60a766d104a387e8dc022cf00de9c54
xiaochun <90xchun@gmail.com>
2025-08-15 11:28:44 +0800

    Use javascript render markdown

--- src/main/java/com/zrlog/install/util/InstallNativeImageResourceUtils.java
+++ src/main/java/com/zrlog/install/util/InstallNativeImageResourceUtils.java
@@ -15,11 +15,11 @@ public class InstallNativeImageResourceUtils {
     public static List<String> getResourceNameList() {
         List<String> resourceNameList = new ArrayList<>();
         resourceNameList.add("/i18n/disclaimer-agreement/en_US.md");
-        resourceNameList.add("/i18n/disclaimer-agreement/en_US.html");
         resourceNameList.add("/i18n/disclaimer-agreement/zh_CN.md");
-        resourceNameList.add("/i18n/disclaimer-agreement/zh_CN.html");
         resourceNameList.add("/i18n/init-blog/en_US.md");
+        resourceNameList.add("/i18n/init-blog/en_US.html");
         resourceNameList.add("/i18n/init-blog/zh_CN.md");
+        resourceNameList.add("/i18n/init-blog/zh_CN.html");
         resourceNameList.add("/i18n/install_en_US.properties");
         resourceNameList.add("/i18n/install_zh_CN.properties");
         try (InputStream assetJson = InstallNativeImageResourceUtils.class.getResourceAsStream("/install/asset-manifest.json")) {
